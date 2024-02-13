b4cb2f7d2c882c8c8fb207c35956fd5791f06968
Hendrik Görner <johndoe@example.com>
2024-02-14 00:55:28 +0100

    ,

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from data.download_osm_pois import create_osm_poi_gdf
-from data.download_osm_area import create_osm_area_gdf
+from data.download_osm.download_osm_pois import create_osm_poi_gdf
+from data.download_osm.download_osm_area import create_osm_area_gdf
 
 #create_osm_area_gdf()
 create_osm_poi_gdf()
