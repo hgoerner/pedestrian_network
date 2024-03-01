c0bdbbc0aa662cc59a6405ac6596abadd777b461
hgoerner <hendrik.goerner@tu-dresden.de>
2024-03-01 17:07:29 +0100

    update montreal

---
+++ Varianten/Montreal/Testbeispiel.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import geopandas as gpd
+import pandas as pd
+from shapely import Point
+
+current_directory = os.getcwd()
+print(current_directory)
+
+sys.path.append('C:\\Users\\Hendr\\OneDrive\\Desktop\\pedestrian_network')
+sys.path.append('C:\\Users\\Goerner\\Desktop\\pedestrian_network')
+
+
+from utils.save_data import safe_gdf_as_gpkg
+
+montreal_counts_csv_filepath = r"data\input\Sonstiges\comptages_vehicules_cyclistes_pietons.csv"
+
+#montreal crs
+crs = "EPSG:32188"
+
+monetreal_counts_df = pd.read_csv(montreal_counts_csv_filepath, sep=",")
+
+#filter only pedestrians
+monetreal_counts_ped_df = monetreal_counts_df[monetreal_counts_df["Description_Code_Banque"] == "Pietons"]
+print(monetreal_counts_ped_df)
+
+# Create a GeoDataFrame with Point geometries
+geometry = [Point(xy) for xy in zip(monetreal_counts_ped_df['Localisation_X'], monetreal_counts_ped_df['Localisation_Y'])]
+
+# return 729 Knotenpunkte
+print(len(monetreal_counts_ped_df["Id_Intersection"].unique()))
+
+# Create a GeoDataFrame with the original DataFrame and the geometry
+monetreal_counts_ped_gdf = gpd.GeoDataFrame(monetreal_counts_ped_df, geometry=geometry, crs=crs).to_crs("EPSG:31468")
+
+safe_gdf_as_gpkg((monetreal_counts_ped_gdf, "intersections_with_counts_montreal"))
+
+#montreal_counts = gpd.read_file()
